5cd87ea002c716fba82e6a59908bc56c380fd35c
CHAO201030 <2227685463@qq.com>
2024-08-22 10:08:55 +0800

    添加Redis缓存 关键词和网页搜索使用不同的Redis数据库

--- wfrest_server/NetServer.cpp
+++ wfrest_server/NetServer.cpp
@@ -65,20 +65,61 @@ void NetServer::keyWordRecommendMoudle()
         }
         else
         {
-            KeyRecommend recommend;
+            WFRedisTask * redis_task = WFTaskFactory::create_redis_task("redis://127.0.0.1:6379", 10,
+            
+            [resp, query_word, series](WFRedisTask * redis_task)
+            {
+                protocol::RedisResponse * redis_resp = redis_task->get_resp();
+                protocol::RedisValue redis_result;
 
-            recommend.doRecommend(query_word);
+                redis_resp->get_result(redis_result);
 
-            string res = recommend.getResult();
+                if(redis_result.is_error() || redis_task->get_state() != WFT_STATE_SUCCESS)
+                {
+                    resp->String("Server Redis Error");
+                }
+                else
+                {
+                    string res = redis_result.string_value();
 
-            resp->String(res);
+                    if(res.empty())
+                    {
+                        KeyRecommend recommend;
+
+                        recommend.doRecommend(query_word);
+
+                        res = recommend.getResult();
+
+                        WFRedisTask * insert_task = WFTaskFactory::create_redis_task("redis://127.0.0.1:6379", 10, nullptr);
+                        
+                        cout << "\n[INFO] : Key Recommend Cache Missing...\n";
+                        
+                        insert_task->get_req()->set_request("SET", {query_word, res});
+                        
+                        series->push_back(insert_task);
+                    }
+
+                    resp->String(res);
+                }
+            }
+            );
+ 
+            redis_task->get_req()->set_request("GET", {query_word});
+            series->push_back(redis_task);
+
+            // KeyRecommend recommend;
+
+            // recommend.doRecommend(query_word);
+
+            // string res = recommend.getResult();
+
+            // resp->String(res);
         }
     });
 }
 
 void NetServer::webPageSearchMoudle()
 {
-
     _server.GET("/search",
     
     [this](const HttpReq * req, HttpResp * resp, SeriesWork * series)
@@ -92,13 +133,57 @@ void NetServer::webPageSearchMoudle()
         }
         else
         {
-            WebSearch search;
-
-            search.doSearch(query_word);
+            WFRedisTask * redis_task = WFTaskFactory::create_redis_task("redis://127.0.0.1:6379/1", 10,
             
-            string res = search.getResult();
+            [resp, query_word, series](WFRedisTask * redis_task)
+            {
+                protocol::RedisResponse * redis_resp = redis_task->get_resp();
+                protocol::RedisValue redis_result;
 
-            resp->String(res);
+                redis_resp->get_result(redis_result);
+
+                if(redis_result.is_error() || redis_task->get_state() != WFT_STATE_SUCCESS)
+                {
+                    resp->String("Server Redis Error");
+                }
+                else
+                {
+                    string res = redis_result.string_value();
+
+                    if(res.empty())
+                    {
+                        WebSearch search;
+
+                        search.doSearch(query_word);
+                        
+                        string res = search.getResult();
+
+                        resp->String(res);
+
+                        WFRedisTask * insert_task = WFTaskFactory::create_redis_task("redis://127.0.0.1:6379/1", 10, nullptr);
+
+                        cout << "\n[INFO] : Web Search Cache Missing...\n";
+
+                        insert_task->get_req()->set_request("SET", {query_word, res});
+                        
+                        series->push_back(insert_task);
+                    }
+
+                    resp->String(res);
+                }
+            }
+            );
+ 
+            redis_task->get_req()->set_request("GET", {query_word});
+            series->push_back(redis_task);
+
+            // WebSearch search;
+
+            // search.doSearch(query_word);
+            
+            // string res = search.getResult();
+
+            // resp->String(res);
         }
     });
 }
